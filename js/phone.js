ff720953af8d1ee0464badfdbb8d0a4b4fe8db85
AthkiaAziza <athkiaaziza@gmail.com>
2024-02-28 12:19:02 +0600

    added modal features

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
 
-const loadPhone = async (searchText, isShowAll) => {
+const loadPhone = async (searchText = '13', isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
@@ -13,22 +13,22 @@ const displayPhones = (phones, isShowAll) => {
     const phoneContainer = document.getElementById('phone-container');
     // clear phone container cards before adding new cards
     phoneContainer.textContent = ''
-    
+
     // display show all button if there are more than 12 phones
     const showAllContainer = document.getElementById('show-all-container')
-    if(phones.length > 12 && !isShowAll){
+    if (phones.length > 12 && !isShowAll) {
         showAllContainer.classList.remove('hidden')
     }
-    else{
+    else {
         showAllContainer.classList.add('hidden')
     }
-    console.log('is show all', isShowAll)
+    // console.log('is show all', isShowAll)
 
     // display first 12 phones if not show all
-    if(!isShowAll){
+    if (!isShowAll) {
         phones = phones.slice(0, 12);
     }
-    
+
 
     phones.forEach(phone => {
         //console.log(phone);
@@ -56,12 +56,32 @@ const displayPhones = (phones, isShowAll) => {
 }
 
 //
-const handleShowDetail = async(id) =>{
-    console.log('clicked show details', id)
+const handleShowDetail = async (id) => {
+    //console.log('clicked show details', id)
     // load single phone data
     const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
     const data = await res.json();
-    console.log(data);
+
+
+    const phone = data.data;
+
+    showPhoneDetails(phone);
+}
+
+const showPhoneDetails = (phone) => {
+    console.log(phone);
+    const phoneName = document.getElementById('show-detail-phone-name');
+    phoneName.innerText = phone.name;
+
+    const showDetailContainer = document.getElementById('show-detail-container');
+
+    showDetailContainer.innerHTML = `
+    <img src="${phone.image}" alt="">
+    <p><span>Storage:</span>${phone?.mainFeatures?.storage}</p>
+    <p><span>GPS:</span>${phone?.others?.GPS || 'No GPS available'}</p>
+    `
+    // show the modal
+    show_details_modal.showModal();
 }
 
 // Handle search button
@@ -73,20 +93,20 @@ const handleSearch = (isShowAll) => {
     loadPhone(searchText, isShowAll);
 }
 
-const toggleLoadingSpinner = (isLoading) =>{
+const toggleLoadingSpinner = (isLoading) => {
     const loadingSpinner = document.getElementById('loading-spinner');
-    if(isLoading){
+    if (isLoading) {
         loadingSpinner.classList.remove('hidden');
     }
-    else{
+    else {
         loadingSpinner.classList.add('hidden');
     }
 }
 
 // handle show all
-const handleShowAll = () =>{
+const handleShowAll = () => {
     handleSearch(true);
 
 }
 
-//loadPhone();
+loadPhone();
